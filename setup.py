d40f69706cedd155721fa3508550e2472c7aa80b
Gardar Thorsteinsson <gardart@gmail.com>
2021-10-20 18:01:21 +0000

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="example-pkg-gardart",
-    version="0.0.1",
+    version="0.0.2",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
