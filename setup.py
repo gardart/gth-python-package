5ade944109fc5dea2f6adadda7ba842ed51f085b
Gardar Thorsteinsson <gardart@gmail.com>
2021-10-22 13:57:02 +0000

    Version upgraded

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gth-timechecker",
-    version="0.0.7",
+    version="0.0.8",
     author="Garðar Þorsteinsson",
     author_email="gardart@gmail.com",
     url="https://example.com",
